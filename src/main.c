572df12528b5f2d72ed59de2c170fc3ed9cedf66
samarjit bhogal <samarjit.v.bhogal@gmail.com>
2024-07-16 13:16:06 -0700

    Setup grid field, cell reveal and nearby mines

--- src/main.c
+++ src/main.c
@@ -1,42 +1,99 @@
+#include <stdlib.h>
+#include <time.h>
+
 #include "raylib.h"
+#include "raymath.h"
 
-//----------------------------------------------------------------------------------
-// Local Variables Definition (local to this module)
-//----------------------------------------------------------------------------------
-Camera camera = { 0 };
-Vector3 cubePosition = { 0 };
+#define COLS 10
+#define ROWS 10
 
-//----------------------------------------------------------------------------------
-// Local Functions Declaration
-//----------------------------------------------------------------------------------
-static void UpdateDrawFrame(void);          // Update and draw one frame
+const int screenWidth = 400;
+const int screenHeight = 400;
 
-//----------------------------------------------------------------------------------
-// Main entry point
-//----------------------------------------------------------------------------------
-int main()
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+const int cellWidth = screenWidth / COLS;
+const int cellHeight = screenHeight / ROWS;
+
+typedef struct cell {
+    int i;
+    int j;
+    bool containsMine;
+    bool revealed;
+    int nearbyMines;
+} Cell;
+
+Cell grid[COLS][ROWS];
+
+void CellDraw(Cell);
+bool IndexIsValid(int, int);
+void CellReveal(int, int);
+int CellCountMines(int, int);
+
+int main() {
+
+    srand(time(0));
 
     InitWindow(screenWidth, screenHeight, "raylib");
-
-    camera.position = (Vector3){ 10.0f, 10.0f, 8.0f };
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
-    camera.fovy = 60.0f;
-    camera.projection = CAMERA_PERSPECTIVE;
 
     //--------------------------------------------------------------------------------------
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
+    for (int i = 0; i < COLS; i++) {
+        for (int j = 0; j < ROWS; j++) {
+            //.i is named member initialization
+            grid[i][j] = (Cell) {
+                .i = i, 
+                .j = j,
+                .containsMine = false,
+                .revealed = false,
+                .nearbyMines = -1
+            };
+        }
+    }
+
+    int minesToPlace = (int) (ROWS * COLS * 0.1f);
+    while (minesToPlace > 0) {
+        int i = rand() % COLS;
+        int j = rand() % ROWS;
+
+        if (!grid[i][j].containsMine) {
+            grid[i][j].containsMine = true;
+            minesToPlace--;
+        }
+    }
+
+    for (int i = 0; i < COLS; i++) {
+        for (int j = 0; j < ROWS; j++) {
+            if (!grid[i][j].containsMine) {
+                grid[i][j].nearbyMines = CellCountMines(i, j);
+            }           
+        }
+    }
+
     // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        UpdateDrawFrame();
+    while (!WindowShouldClose()) {
+        
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            Vector2 mPos = GetMousePosition();
+            int indexI = mPos.x / cellWidth;
+            int indexJ = mPos.y / cellHeight;
+
+            if (IndexIsValid(indexI, indexJ)) {
+                CellReveal(indexI, indexJ);
+            }
+        }
+
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            for (int i = 0; i < COLS; i++) {
+                for (int j = 0; j < ROWS; j++) {
+                    CellDraw(grid[i][j]);
+                }
+            }
+
+        EndDrawing();
     }
 
     // De-Initialization
@@ -47,32 +104,49 @@ int main()
     return 0;
 }
 
-// Update and draw game frame
-static void UpdateDrawFrame(void)
-{
-    // Update
-    //----------------------------------------------------------------------------------
-    UpdateCamera(&camera, CAMERA_ORBITAL);
-    //----------------------------------------------------------------------------------
+void CellDraw(Cell cell) {
+    if (cell.revealed) {
+        if (cell.containsMine) {
+            DrawRectangle(cell.i * cellWidth, cell.j * cellHeight, cellWidth, cellHeight, RED);
+        } else {
+            DrawRectangle(cell.i * cellWidth, cell.j * cellHeight, cellWidth, cellHeight, LIGHTGRAY);
+            DrawText(TextFormat("%d", cell.nearbyMines), cell.i * cellWidth +6, cell.j * cellHeight +4, cellHeight - 8, DARKGRAY);
+        }
+    }
 
-    // Draw
-    //----------------------------------------------------------------------------------
-    BeginDrawing();
+    DrawRectangleLines(cell.i * cellWidth, cell.j * cellHeight, cellWidth, cellHeight, BLACK);
+}
 
-        ClearBackground(RAYWHITE);
+bool IndexIsValid(int i, int j) {
+    return i >= 0 && i < COLS && j >=0 && j < ROWS;
+}
 
-        BeginMode3D(camera);
+void CellReveal(int i, int j) {
+    grid[i][j].revealed = true;
 
-            DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
-            DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
-            DrawGrid(10, 1.0f);
+    if (grid[i][j].containsMine) {
+        ///lose
+    } else {
+        //play sound
+    }
+}
 
-        EndMode3D();
+int CellCountMines(int i, int j) {
+    int count = 0;
 
-        DrawText("This is a raylib example", 10, 40, 20, DARKGRAY);
+    for (int iOff =-1; iOff <= 1; iOff++) {
+        for (int jOff =-1; jOff <= 1; jOff++) {
+            if (iOff == 0 && jOff == 0) {
+                continue;
+            }
+            if (!IndexIsValid(i + iOff, j + jOff)) {
+               continue;
+            } 
+            if (grid[i + iOff][j + jOff].containsMine) {
+                count++;
+            }
+        }
+    }
 
-        DrawFPS(10, 10);
-
-    EndDrawing();
-    //----------------------------------------------------------------------------------
+    return count;
 }
